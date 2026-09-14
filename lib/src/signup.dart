2a21b1827145b833389726a6f175e0cab0a955cf
taeyu <taeyul0821@gmail.com>
2026-09-14 13:53:36 +0900

    회원가입 기능 수정

--- lib/src/signup.dart
+++ lib/src/signup.dart
@@ -9,14 +9,23 @@ class Signup extends StatelessWidget {
       appBar: AppBar(
         title: const Text('계정만들기'),
       ),
-      body: const Center(
-        child: TextField(
+      body: Column(
+        children: [
+          TextField(
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             labelText: '이름을 입력하세요',
           ),
-        )
-      ),
+        ),
+        SafeArea(child: Text('이메일'),),
+         TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: '이메일주소',
+          ),
+        ),
+        ],
+      )
     );
   }
 }
